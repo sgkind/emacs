a99d6aad2cc541d56a1609de6c4e851059d33e5b
sgkind <sunguanke11@mails.ucas.edu.cn>
2020-12-02 07:52:20 +0800

    first commit

---
+++ init.el
@@ -0,0 +1,26 @@
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+(require 'init-elpa)
+(require 'init-ui)
+(require 'init-editing)
+(require 'init-navigation)
+(require 'init-miscellaneous)
+(require 'init-company-mode)
+(require 'init-rust)
+
+(provide 'init)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (flycheck rainbow-delimiters golden-ratio atom-one-dark-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ lisp/init-rust.el
@@ -0,0 +1,10 @@
+;;; rust
+(require 'init-elpa)
+(require-package 'company)
+(require-package 'rust-mode)
+(require-package 'flycheck)
+(require-package 'flycheck-rust)
+
+(require 'rust-mode)
+
+(provide 'init-rust)
